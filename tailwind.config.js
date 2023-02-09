f29f96dec592c89d5b9721f6bd78876b52257521
mahfantuci <maria@imaginationmedia.com>
2023-02-08 21:36:47 -0300

    Configured tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./assets/js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+				"purple-800": "#271A45",
+        "purple-100": "#D9CDF7",
+
+			},
+			fontFamily: {
+				crimson: ["Crimson Pro", "serif"],
+				lato: ["Lato", "serif"],
+			}
+    },
+  },
+  plugins: [],
+}
